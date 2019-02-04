38d7837a84cebb637e8a872c18995812dc64fde7
Arafat Hasan <md.arafathasan@gmail.com>
2019-02-04 11:57:29 +0600

    04-02-2019, auto-commit

---
+++ Contests/Codeforces/1111/1111B.cpp
@@ -0,0 +1,149 @@
+/*
+ * FILE: 1111B.cpp
+ *
+ * @author: Arafat Hasan Jenin <opendoor.arafat[at]gmail[dot]com>
+ *
+ * LINK: https://codeforces.com/contest/1111/problem/B
+ *
+ * DATE CREATED: 03-02-19 21:52:43 (+06)
+ * LAST MODIFIED: 03-02-19 22:58:22 (+06)
+ *
+ * VERDICT: Accepetd
+ *
+ * DEVELOPMENT HISTORY:
+ * Date         Version     Description
+ * --------------------------------------------------------------------
+ * 03-02-19     1.0         Deleted code is debugged code.
+ *
+ *               _/  _/_/_/_/  _/      _/  _/_/_/  _/      _/
+ *              _/  _/        _/_/    _/    _/    _/_/    _/
+ *             _/  _/_/_/    _/  _/  _/    _/    _/  _/  _/
+ *      _/    _/  _/        _/    _/_/    _/    _/    _/_/
+ *       _/_/    _/_/_/_/  _/      _/  _/_/_/  _/      _/
+ */
+
+///////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <cctype>
+#include <cstdio>
+#include <cstdlib>
+#include <iomanip>
+#include <utility>
+#include <sstream>
+#include <algorithm>
+#include <stack>
+#include <set>
+#include <list>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <deque>
+#include <vector>
+#include <tuple>
+#include <stdint.h> //uint32_t
+#include <functional>
+#include <bitset>
+#include <unistd.h> // unsigned int sleep(unsigned int seconds);
+
+using namespace std;
+
+typedef long long           ll;
+typedef double              lf;
+typedef long double         llf;
+typedef unsigned long long  ull;
+typedef pair<int, int>      pii;
+typedef vector<pii>         vpii;
+typedef vector<int>         vi;
+typedef vector<long long>   vl;
+
+#define _USE_MATH_DEFINES
+
+#define forr(i, a, b)   for (__typeof (a) i = (a); i <= (b); i++)
+#define rof(i, b, a)    for (__typeof (a) i = (b); i >= (a); i--)
+#define rep(i, n)       for (__typeof (n) i = 0; i < (n); i++)
+#define forit(i, s)     for (__typeof ((s).end()) i = (s).begin(); i != (s).end(); ++i)
+#define all(ar)         ar.begin(), ar.end()
+#define fill(a, val)    memset((a), (val), sizeof((a)))
+#define clr(a)          memset((a), 0, sizeof((a)))
+#define sz(a)           (int) a.size()
+
+#define pb              push_back
+
+#ifndef ONLINE_JUDGE
+    #define nl              cerr << '\n'
+    #define sp              cerr << ' '
+    #define ckk             cerr << "###############\n"
+    #define debug1(x)       cerr << #x << ": " << (x) << '\n'
+    #define debug2(x, y)    cerr << #x << ": " << (x) << '\t' << #y << ": " << (y) << '\n'
+    #define debug3(x, y, z) cerr << #x << ": " << (x) << '\t' << #y << ": " << (y) << '\t' << #z << ": " << (z) << '\n'
+#else
+    #define nl
+    #define sp
+    #define ckk
+    #define debug1(x)
+    #define debug2(x, y)
+    #define debug3(x, y, z)
+#endif
+
+#define PI              acos(-1.0)
+#define INF             0x7fffffff
+#define MOD             1000000007
+#define EPS             1e-7
+#define MAX             10000007 //1e7+7
+
+////////////////////////// START HERE //////////////////////////
+
+int main() {
+    ios_base::sync_with_stdio (false); cin.tie (0); cout.tie (0);
+    int n, k, m;
+    cin >> n >> k >> m;
+    vi v (n);
+    rep (i, n) {
+        cin >> v[i];
+    }
+    sort (all (v), greater<int>());
+    llf mx = -1e18;
+    llf sum = 0;
+
+    for (int i = 0; i < n; i++) {
+        sum += v[i];
+        int baki = m - (n - (i + 1));
+
+        if (baki < 0) continue;
+
+        if (baki >= 0) {
+            int perHead = baki / (i + 1);
+            int nit = 0;
+
+            if (perHead < k) {
+                nit = perHead * (i + 1);
+
+                if (nit < baki) {
+                    nit += (baki - nit);
+                }
+
+            } else if (perHead == k) {
+                nit = perHead * ( i + 1);
+
+            } else {
+                perHead = k;
+                nit = perHead * (i + 1);
+            }
+
+            llf localSum = sum + nit;
+            llf avg = localSum / (i + 1);
+
+            if (avg - mx > 0.0) mx = avg;
+
+        } else break;
+    }
+
+    cout << fixed << setprecision (12);
+    cout << mx << endl;
+    return 0;
+}

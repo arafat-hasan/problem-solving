b7eca24ea9eaf7aaf1ba6f87fe9407158a237d69
Arafat Hasan <md.arafathasan@gmail.com>
2019-02-12 14:46:40 +0600

    12-02-2019, auto-commit

---
+++ UVa/AC/Marks_Distribution-10910.cpp
@@ -0,0 +1,139 @@
+/*
+ * FILE: Marks_Distribution-10910.cpp
+ *
+ * @author: Arafat Hasan Jenin <opendoor.arafat[at]gmail[dot]com>
+ *
+ * LINK: https://uva.onlinejudge.org/external/109/10910.pdf
+ *
+ * DATE CREATED: 12-02-19 10:55:17 (+06)
+ * LAST MODIFIED: 12-02-19 11:41:10 (+06)
+ *
+ * VERDICT: Accepetd
+ *
+ * DEVELOPMENT HISTORY:
+ * Date         Version     Description
+ * --------------------------------------------------------------------
+ * 12-02-19     1.0         Deleted code is debugged code.
+ *
+ *               _/  _/_/_/_/  _/      _/  _/_/_/  _/      _/
+ *              _/  _/        _/_/    _/    _/    _/_/    _/
+ *             _/  _/_/_/    _/  _/  _/    _/    _/  _/  _/
+ *      _/    _/  _/        _/    _/_/    _/    _/    _/_/
+ *       _/_/    _/_/_/_/  _/      _/  _/_/_/  _/      _/
+ */
+
+///////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <cctype>
+#include <cstdio>
+#include <cstdlib>
+#include <iomanip>
+#include <utility>
+#include <sstream>
+#include <algorithm>
+#include <stack>
+#include <set>
+#include <list>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <deque>
+#include <vector>
+#include <tuple>
+#include <stdint.h> //uint32_t
+#include <functional>
+#include <bitset>
+#include <unistd.h> // unsigned int sleep(unsigned int seconds);
+
+using namespace std;
+
+typedef long long           ll;
+typedef double              lf;
+typedef long double         llf;
+typedef unsigned long long  ull;
+typedef pair<int, int>      pii;
+typedef vector<pii>         vpii;
+typedef vector<int>         vi;
+typedef vector<long long>   vl;
+
+#define _USE_MATH_DEFINES
+
+#define forr(i, a, b)   for (__typeof (a) i = (a); i <= (b); i++)
+#define rof(i, b, a)    for (__typeof (a) i = (b); i >= (a); i--)
+#define rep(i, n)       for (__typeof (n) i = 0; i < (n); i++)
+#define forit(i, s)     for (__typeof ((s).end()) i = (s).begin(); i != (s).end(); ++i)
+#define all(ar)         ar.begin(), ar.end()
+#define fill(a, val)    memset((a), (val), sizeof((a)))
+#define clr(a)          memset((a), 0, sizeof((a)))
+#define sz(a)           (int) a.size()
+
+#define pb              push_back
+
+#ifndef ONLINE_JUDGE
+    #define nl              cerr << '\n'
+    #define sp              cerr << ' '
+    #define ckk             cerr << "###############\n"
+    #define debug1(x)       cerr << #x << ": " << (x) << '\n'
+    #define debug2(x, y)    cerr << #x << ": " << (x) << '\t' << #y << ": " << (y) << '\n'
+    #define debug3(x, y, z) cerr << #x << ": " << (x) << '\t' << #y << ": " << (y) << '\t' << #z << ": " << (z) << '\n'
+#else
+    #define nl
+    #define sp
+    #define ckk
+    #define debug1(x)
+    #define debug2(x, y)
+    #define debug3(x, y, z)
+#endif
+
+#define PI              acos(-1.0)
+#define INF             0x7fffffff
+#define MOD             1000000007
+#define EPS             1e-7
+#define MAX             10000007 //1e7+7
+
+////////////////////////// START HERE //////////////////////////
+
+int cnt, t, p, n;
+int dp[7001][71];
+
+int call (int total, int d) {
+    if (d > n) return 0;
+
+    if (dp[total][d] != -1) return dp[total][d];
+
+    if (d == n and total == t) {
+        return 1;
+    }
+
+    int sum = 0;
+
+    for (int i = p; i <= 70; i++) {
+        int bar = total + i;
+
+        if (bar > t) break;
+
+        sum += call (bar, d + 1);
+    }
+
+    return dp[total][d] = sum;
+}
+
+int main() {
+    ios_base::sync_with_stdio (false); cin.tie (0); cout.tie (0);
+    int tc;
+    cin >> tc;
+
+    while (tc--) {
+        cin >> n >> t >> p;
+        cnt = 0;
+        fill (dp, -1);
+        cout << call (0, 0) << endl;
+    }
+
+    return 0;
+}

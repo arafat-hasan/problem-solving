337869a3bd97889e49c22d4eeb0262c20eeb44f5
Arafat Hasan Jenin <opendoor.arafat@gmail.com>
2019-04-10 21:12:44 +0600

    10 april, 2019

--- Contests/Varity/vjudge/289974/H.cpp
+++ Contests/Varity/vjudge/289974/H.cpp
@@ -1,12 +1,12 @@
 /*
- * FILE: {{untitled}}
+ * FILE: {H.cpp}
  *
  * @author: Arafat Hasan Jenin <arafathasanjenin[at]gmail[dot]com>
  *
  * LINK:
  *
  * DATE CREATED: {{long_date}}
- * LAST MODIFIED: __last_modified
+ * LAST MODIFIED: 27-03-19 23:05:00 (+06)
  *
  * DESCRIPTION:
  *
@@ -63,6 +63,8 @@ typedef vector<long long>   vl;
 
 #define _USE_MATH_DEFINES
 
+#define _FastIO        ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+
 #define forr(i, a, b)   for (__typeof (a) i = (a); i <= (b); i++)
 #define rof(i, b, a)    for (__typeof (a) i = (b); i >= (a); i--)
 #define rep(i, n)       for (__typeof (n) i = 0; i < (n); i++)
@@ -98,8 +100,60 @@ typedef vector<long long>   vl;
 
 ////////////////////////// START HERE //////////////////////////
 
-int main() {
-	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-    return 0;
+
+int dp[1010][1010];
+string str1, str2, str3;
+
+int lcs (string &X, string &Y, int m, int n ) {
+    if (m == 0 || n == 0)
+        return 0;
+
+    if (dp[m][n] != -1)
+        return dp[m][n];
+
+    if (X[m - 1] == Y[n - 1])
+        return dp[m][n] = 1 + lcs (X, Y, m - 1, n - 1);
+    else
+        return dp[m][n] = max (lcs (X, Y, m, n - 1), lcs (X, Y, m - 1, n) );
 }
 
+
+// dp array needed, last to first
+string lcsAns; // store lcs here
+string lcs_print (string X, string Y, int m, int n) {
+    if (m == -1 or n == -1) {
+        reverse (all (lcsAns) );
+        return lcsAns;
+    }
+
+    if (X[m - 1] == Y[n - 1]) {
+        lcsAns += X[m - 1];
+        lcs_print (X, Y, m - 1, n - 1);
+
+    } else {
+        if (dp[m - 1][n] > dp[m][n - 1]) lcs_print (X, Y, m - 1, n);
+        else lcs_print (X, Y, m, n - 1);
+    }
+
+    return lcsAns;
+}
+
+int main() {
+    _FastIO;
+    int t, cs = 0;
+    cin >> t;
+
+    while (t--) {
+        cin.ignore();
+        cin >> str1 >> str2 >> str3;
+        fill (dp, -1);
+        lcsAns.clear();
+        lcs (str1, str2, (int) str1.size(), (int) str2.size());
+        string lc = lcs_print (str1, str2, (int) str1.size(), (int) str2.size());
+        fill (dp, -1);
+        int sec = lcs (lc, str3, (int) lc.size(), (int) str3.size());
+        cout << "Case " << ++cs << ": " << sec << endl;
+    }
+
+    return 0;
+}

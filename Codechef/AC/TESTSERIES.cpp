95a8c1510338ce655ffa5dbbec87a0944dbf7ff6
Arafat Hasan <opendoor.arafat@gmail.com>
2021-10-16 19:43:28 +0600

    started after a long

---
+++ Codechef/AC/TESTSERIES.cpp
@@ -0,0 +1,107 @@
+/*
+ * FILE: TESTSERIES.cpp
+ * @author: Arafat Hasan Jenin <opendoor.arafat[at]gmail[dot]com>
+ * LINK:
+ * DATE CREATED: 15-10-21 21:58:22 (+06)
+ * LAST MODIFIED: 15-10-21 22:05:11 (+06)
+ */
+
+#include <stdint.h>  //uint32_t
+#include <unistd.h>  // unsigned int sleep(unsigned int seconds);
+
+#include <algorithm>
+#include <bitset>
+#include <cctype>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>  // std::istream_iterator
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+typedef double lf;
+typedef long double llf;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef vector<pii> vpii;
+typedef vector<int> vi;
+typedef vector<long long> vl;
+
+#define _USE_MATH_DEFINES
+
+#define forr(i, a, b) for (__typeof(a) i = (a); i <= (b); i++)
+#define rof(i, b, a) for (__typeof(a) i = (b); i >= (a); i--)
+#define rep(i, n) for (__typeof(n) i = 0; i < (n); i++)
+#define forit(i, s) \
+  for (__typeof((s).end()) i = (s).begin(); i != (s).end(); ++i)
+#define all(ar) ar.begin(), ar.end()
+#define fill(a, val) memset((a), (val), sizeof((a)))
+#define clr(a) memset((a), 0, sizeof((a)))
+#define sz(a) (int)a.size()
+
+#define pb push_back
+
+#ifndef ONLINE_JUDGE
+#define nl cerr << '\n'
+#define sp cerr << ' '
+#define ckk cerr << "###############\n"
+#define debug1(x) cerr << #x << ": " << (x) << '\n'
+#define debug2(x, y) \
+  cerr << #x << ": " << (x) << '\t' << #y << ": " << (y) << '\n'
+#define debug3(x, y, z)                                                        \
+  cerr << #x << ": " << (x) << '\t' << #y << ": " << (y) << '\t' << #z << ": " \
+       << (z) << '\n'
+#else
+#define nl
+#define sp
+#define ckk
+#define debug1(x)
+#define debug2(x, y)
+#define debug3(x, y, z)
+#endif
+
+#define PI acos(-1.0)
+#define INF 0x7fffffff
+#define MOD 1000000007
+#define EPS 1e-7
+#define MAX 10000007  // 1e7+7
+
+////////////////////////// START HERE //////////////////////////
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+  int t;
+  cin >> t;
+  while (t--) {
+    int india = 0, england = 0, tmp;
+    for (int i = 0; i < 5; ++i) {
+      cin >> tmp;
+      if (tmp == 1) {
+        india++;
+      } else if (tmp == 2) {
+        england++;
+      }
+    }
+    cout << ((india == england)? "DRAW" : ((india > england)? "INDIA" : "ENGLAND")) << '\n';
+  }
+  return 0;
+}

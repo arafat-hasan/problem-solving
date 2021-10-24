44b7d25dac6fa35d85560e3cf9db166b30cc35c1
Arafat Hasan <opendoor.arafat@gmail.com>
2021-10-25 00:36:27 +0600

    25 oct

---
+++ leetcode/AC/Medium/insert-delete-getrandom-o1.cpp
@@ -0,0 +1,126 @@
+/*
+ * FILE: insert-delete-getrandom-o1.cpp
+ * @author: Arafat Hasan Jenin <opendoor.arafat[at]gmail[dot]com>
+ * LINK: https://leetcode.com/problems/insert-delete-getrandom-o1
+ * DATE CREATED: 21-10-21 17:56:28 (+06)
+ * LAST MODIFIED: 21-10-21 18:36:07 (+06)
+ * VERDICT: Accepetd
+ */
+
+#include <stdint.h>  //uint32_t
+#include <unistd.h>  // unsigned int sleep(unsigned int seconds);
+
+#include <algorithm>
+#include <bitset>
+#include <cctype>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>  // std::istream_iterator
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+typedef double lf;
+typedef long double llf;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef vector<pii> vpii;
+typedef vector<int> vi;
+typedef vector<long long> vl;
+
+#define _USE_MATH_DEFINES
+
+#define forr(i, a, b) for (__typeof(a) i = (a); i <= (b); i++)
+#define rof(i, b, a) for (__typeof(a) i = (b); i >= (a); i--)
+#define rep(i, n) for (__typeof(n) i = 0; i < (n); i++)
+#define forit(i, s) \
+  for (__typeof((s).end()) i = (s).begin(); i != (s).end(); ++i)
+#define all(ar) ar.begin(), ar.end()
+#define fill(a, val) memset((a), (val), sizeof((a)))
+#define clr(a) memset((a), 0, sizeof((a)))
+#define sz(a) (int)a.size()
+
+#define pb push_back
+
+#ifndef ONLINE_JUDGE
+#define nl cerr << '\n'
+#define sp cerr << ' '
+#define ckk cerr << "###############\n"
+#define debug1(x) cerr << #x << ": " << (x) << '\n'
+#define debug2(x, y) \
+  cerr << #x << ": " << (x) << '\t' << #y << ": " << (y) << '\n'
+#define debug3(x, y, z)                                                        \
+  cerr << #x << ": " << (x) << '\t' << #y << ": " << (y) << '\t' << #z << ": " \
+       << (z) << '\n'
+#else
+#define nl
+#define sp
+#define ckk
+#define debug1(x)
+#define debug2(x, y)
+#define debug3(x, y, z)
+#endif
+
+#define PI acos(-1.0)
+#define INF 0x7fffffff
+#define MOD 1000000007
+#define EPS 1e-7
+#define MAX 10000007  // 1e7+7
+
+class RandomizedSet {
+ public:
+  RandomizedSet() {}
+
+  bool insert(int val) {
+    if (hash.find(val) != hash.end()) {
+      return false;
+    }
+
+    hash[val] = list.size();
+    list.push_back(val);
+    return true;
+  }
+
+  bool remove(int val) {
+    if (hash.find(val) == hash.end()) {
+      return false;
+    }
+
+    swap(list[hash[val]], list[list.size() - 1]);
+    hash[list[hash[val]]] = hash[val];
+    list.pop_back();
+    hash.erase(val);
+    return true;
+  }
+
+  int getRandom() { return list[rand() % list.size()]; }
+
+ private:
+  map<int, int> hash;
+  vector<int> list;
+};
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet* obj = new RandomizedSet();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */
